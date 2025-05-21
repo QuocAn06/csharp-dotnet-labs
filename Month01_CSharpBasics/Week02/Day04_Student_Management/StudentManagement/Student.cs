81fa71354446a8825162ce871a1cc84d007b37e8
Le Nguyen Quoc An <lenguyenquocan116@gmail.com>
2025-05-21 22:36:29 +0700

    SE04 - create Student Class

---
+++ Month01_CSharpBasics/Week02/Day04_Student_Management/StudentManagement/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementProject
+{
+    public class Student
+    {
+        //Properties
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double GPA { get; set; }
+
+        //Default constructor
+        public Student() { }
+
+        //Parameterized constructor
+        public Student(string id, string name, int age, double gpa) 
+        { 
+            ID = id;
+            Name = name;
+            Age = age;
+            GPA = gpa;
+        }
+
+        //Method to display student info
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"ID: {ID}, Name: {Name}, Age: {Age}, GPA: {GPA}");
+        }
+    }
+}
---
+++ Month01_CSharpBasics/Week02/Day04_Student_Management/StudentManagement/StudentManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementProject
+{
+    public class StudentManager
+    {
+        private List<Student> students = new List<Student>();
+
+        //Add new student
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+            Console.WriteLine("Student added successfully!");
+        }
+
+        //Display all students
+        public void DisplayAllStudents()
+        {
+            Console.WriteLine("\nList of Student: ");
+            foreach (var student in students)
+            {
+                student.DisplayInfo();
+            }
+        }
+
+        //Calculate average GPA
+        public double CalulateAverageGPA()
+        {
+            if (students.Count == 0)
+                return 0;
+
+            double totalGPA = 0;
+            foreach (var student in students)
+            {
+                totalGPA += student.GPA;
+            }
+            return totalGPA / students.Count;
+        }
+    }
+}
