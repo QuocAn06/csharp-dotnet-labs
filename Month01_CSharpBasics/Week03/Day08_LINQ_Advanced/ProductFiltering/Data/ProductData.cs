1ae9f1c9a83300dd2b77a33fc12cda05f43d4327
Le Nguyen Quoc An <lenguyenquocan116@gmail.com>
2025-06-17 22:47:24 +0700

    create function FilterProducts

---
+++ Month01_CSharpBasics/Week03/Day08_LINQ_Advanced/ProductFiltering/Data/ProductData.cs
@@ -0,0 +1,28 @@
+﻿using ProductFiltering.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductFiltering.Data
+{
+    public static class ProductData
+    {
+        public static List<Product> GetSampleProducts()
+        {
+            return new List<Product> {
+                new Product { Name = "iPhone 14", Category = "Electronics", Price = 21000000, InStock = 5 },
+                new Product { Name = "Dell Laptop", Category = "Electronics", Price = 18000000, InStock = 3 },
+                new Product { Name = "LG Washing Machine", Category = "Home Appliances", Price = 9500000, InStock = 2 },
+                new Product { Name = "Samsung Refrigerator", Category = "Home Appliances", Price = 12500000, InStock = 0 },
+                new Product { Name = "Men's T-Shirt", Category = "Fashion", Price = 250000, InStock = 12 },
+                new Product { Name = "Basic C# Book", Category = "Books", Price = 150000, InStock = 7 },
+                new Product { Name = "Sony 55\" TV", Category = "Electronics", Price = 15500000, InStock = 4 },
+                new Product { Name = "Rice Cooker", Category = "Home Appliances", Price = 800000, InStock = 10 },
+                new Product { Name = "Sports Shoes", Category = "Fashion", Price = 1200000, InStock = 6 },
+                new Product { Name = "LINQ Learning Book", Category = "Books", Price = 220000, InStock = 5 }
+            };
+        }
+    }
+}
---
+++ Month01_CSharpBasics/Week03/Day08_LINQ_Advanced/ProductFiltering/Services/ProductService.cs
@@ -0,0 +1,33 @@
+﻿using ProductFiltering.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductFiltering.Services
+{
+    public static class ProductService
+    {
+        // 1. Lọc sản phẩm theo loại, khoảng giá, còn hàng
+        public static List<Product> FilterProducts(List<Product> products, string category, decimal minPrice, decimal maxPrice)
+        {
+            var listResult = products.Where(p => p.Category.Equals(category, StringComparison.OrdinalIgnoreCase)
+                                                 && p.Price >= minPrice
+                                                 && p.Price <= maxPrice
+                                                 && p.InStock >0)
+                                     .ToList();
+
+            return listResult;
+
+        }
+
+        // 2. Gom nhóm theo loại và đếm số lượng
+
+        // 3. Tính tổng tiền hàng còn tồn kho
+
+        // 4. Liệt kê Top 3 sản phẩm có giá cao nhất
+
+
+    }
+}
